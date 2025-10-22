32fa59011e4f58cd339a18273ef7d8f9b652db81
adrian <adrian.toma006@gmail.com>
2025-10-23 00:38:51 +0100

    QOL

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,11 @@ import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <HelmetProvider>
         <App />
-      </HelmetProvider>
       <Analytics />
       <SpeedInsights />
     </Router>
